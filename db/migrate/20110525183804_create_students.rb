477819145c1ea40b96286be17d8c48d5b1495e45
Anthony Lewis <me@anthonylewis.com>
2011-05-25 13:39:41 -0500

    Adding students

---
+++ db/migrate/20110525183804_create_students.rb
@@ -0,0 +1,16 @@
+class CreateStudents < ActiveRecord::Migration
+  def self.up
+    create_table :students do |t|
+      t.string :name
+      t.string :email
+      t.string :url
+      t.text :bio
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :students
+  end
+end
