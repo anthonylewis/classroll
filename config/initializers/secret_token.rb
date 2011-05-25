9e7da4db225e7aea3cd5d33487100afa30f00357
Anthony Lewis <me@anthonylewis.com>
2011-05-25 13:27:38 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Classroll::Application.config.secret_token = '6d4e91618c095fe7d7576ec394a9b3f81015acd4bb0a404060d42c6a5b10e7e1aa5ffcb3ec5c657d96f498b79e9b506154e75f2f6c7538388be544df2f7cd680'
